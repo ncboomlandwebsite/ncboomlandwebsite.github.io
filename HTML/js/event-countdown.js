9a3376d34332b9fa17c7bd2cf3fc0e566c1a6c4f
aneeshkarpoor <aneeshkarpoor@gmail.com>
2019-03-01 20:25:06 -0800

    v1

---
+++ HTML/js/event-countdown.js
@@ -0,0 +1,20 @@
+var deadline = new Date(2019, 11, 31, 11, 59, 59).getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var t = deadline - now;
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  if (t < 0) {
+    document.getElementById("days").textContent = "0";
+    document.getElementById("hours").textContent = "0";
+    document.getElementById("minutes").textContent = "0";
+    document.getElementById("seconds").textContent = "0";
+  }
+}, 1000);
